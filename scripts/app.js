33e1a88a324ff661dea3ba0c9d5e061e43cbce9d
Tammy <tammy.n.tran@gmail.com>
2021-03-09 18:52:19 -0500

    implement jQuery to slide aside content

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,45 @@
 // JQUERY TO MAKE INTERACTIVE
 // 1. when about or project is clicked, change the landing info and show content
 // dont forget to add script line in html
+
+$(document).ready(function() {
+
+  $('#about-me').on('click', () => {
+    const sideWidth = $('.about-me-info').width();
+
+    if(sideWidth === 0) {
+      $('.about-me-info').animate({
+        width: '400px'
+      },1000)
+      $('.about-me-info').css('visibility', 'visible')
+    } else {
+      $('.about-me-info').animate({
+        width: 0
+      },1000, function() {
+        $('.about-me-info').css('visibility', 'hidden')
+      })
+    }
+  })
+
+  $('#projects').on('click', () => {
+    const sideWidth = $('.projects-info').width();
+
+    if(sideWidth === 0) {
+      $('.projects-info').animate({
+        width: '400px'
+      },1000)
+      $('.projects-info').css('visibility', 'visible')
+    } else {
+      $('.projects-info').animate({
+        width: 0
+      },1000, function() {
+        $('.projects-info').css('visibility', 'hidden')
+      })
+    }
+  })
+
+
+
+
+
+})
