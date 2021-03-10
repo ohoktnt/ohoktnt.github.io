478e9b3e876487c5c9e7703ff627d4abbcbf7163
Tammy <tammy.n.tran@gmail.com>
2021-03-09 20:44:53 -0500

    fixed double call bug

--- scripts/app.js
+++ scripts/app.js
@@ -6,40 +6,80 @@ $(document).ready(function() {
 
   $('#about-me').on('click', () => {
     const sideWidth = $('.about-me-info').width();
+    const sideWidthP = $('.projects-info').width();
 
-    if(sideWidth === 0) {
-      $('.about-me-info').animate({
-        width: '400px'
-      },1000)
-      $('.about-me-info').css('visibility', 'visible')
-    } else {
-      $('.about-me-info').animate({
-        width: 0
-      },1000, function() {
-        $('.about-me-info').css('visibility', 'hidden')
-      })
-    }
-  })
-
-  $('#projects').on('click', () => {
-    const sideWidth = $('.projects-info').width();
-
-    if(sideWidth === 0) {
-      $('.projects-info').animate({
-        width: '400px'
-      },1000)
-      $('.projects-info').css('visibility', 'visible')
-    } else {
+    if(sideWidthP !== 0) {
       $('.projects-info').animate({
         width: 0
       },1000, function() {
         $('.projects-info').css('visibility', 'hidden')
+        if(sideWidth === 0) {
+          $('.about-me-info').animate({
+            width: '50%'
+          },1000)
+          $('.about-me-info').css('visibility', 'visible')
+        } else {
+          $('.about-me-info').animate({
+            width: 0
+          },1000, function() {
+            $('.about-me-info').css('visibility', 'hidden')
+          })
+        }
       })
+    } else {
+      if(sideWidth === 0) {
+        $('.about-me-info').animate({
+          width: '50%'
+        },1000)
+        $('.about-me-info').css('visibility', 'visible')
+      } else {
+        $('.about-me-info').animate({
+          width: 0
+        },1000, function() {
+          $('.about-me-info').css('visibility', 'hidden')
+        })
+      }
+
     }
+
   })
 
-
-
-
+  $('#projects').on('click', () => {
+    const sideWidth = $('.projects-info').width();
+    const sideWidthA = $('.about-me-info').width();
+    
+    if(sideWidthA !== 0) {
+      $('.about-me-info').animate({
+        width: 0
+      },1000, function() {
+        $('.about-me-info').css('visibility', 'hidden')
+        if(sideWidth === 0) {
+          $('.projects-info').animate({
+            width: '50%'
+          },1000)
+          $('.projects-info').css('visibility', 'visible')
+        } else {
+          $('.projects-info').animate({
+            width: 0
+          },1000, function() {
+            $('.projects-info').css('visibility', 'hidden')
+          })
+        }
+      })
+    } else {
+      if(sideWidth === 0) {
+        $('.projects-info').animate({
+          width: '50%'
+        },1000)
+        $('.projects-info').css('visibility', 'visible')
+      } else {
+        $('.projects-info').animate({
+          width: 0
+        },1000, function() {
+          $('.projects-info').css('visibility', 'hidden')
+        })
+      }
+    }    
+  })
 
 })
